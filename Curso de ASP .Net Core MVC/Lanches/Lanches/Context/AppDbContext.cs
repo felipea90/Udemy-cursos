742460fa164940bcc2ed1a05de8f54ce10946c6b
Felipe Amaral <felipe.abcosta90@gmail.com>
2021-07-16 17:49:57 -0300

    .Net mvc

---
+++ Curso de ASP .Net Core MVC/Lanches/Lanches/Context/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Lanches.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lanches.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Lanche> Lanches { get; set; }
+
+        public DbSet<Categoria> Categorias { get; set; }
+    }
+}
---
+++ Curso de ASP .Net Core MVC/Lanches/Lanches/Models/Categoria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lanches.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+
+        [StringLength(100)]
+        public string Descricao { get; set; }
+    }
+}
