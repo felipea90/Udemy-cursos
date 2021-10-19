b5fea9f1141ff08fa20e973e7411197f6626a1b3
Felipe_Mag3s <felipe@mag3s.com>
2021-10-19 17:58:51 -0300

    webapi.identity

---
+++ Balta.IO/MeuToDo/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using MeuToDo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeuToDo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlite(connectionString: "DataSource=app.db;Cache=Shared");
+
+    }
+}
---
+++ Curso Asp.NET Core Identity + MVC e Web API + SQL Server + EF Core/SOL.Identity/WebAPI.Repository/Context.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using WebAPI.Domain;
+
+namespace WebAPI.Repository
+{
+    public class Context : IdentityDbContext<User>
+    {
+        public Context(DbContextOptions<Context> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<UserRole>(userRole =>
+            {
+                userRole.HasKey(ur => new { ur.UserId, ur.RoleId });
+            });
+
+            builder.Entity<Organization>(org =>
+            {
+                org.ToTable("Organizations");
+                org.HasKey(x => x.Id);
+
+                org.HasMany<User>()
+                .WithOne()
+                .HasForeignKey(x => x.OrgId)
+                .IsRequired(false);
+            });
+        }
+    }
+}
